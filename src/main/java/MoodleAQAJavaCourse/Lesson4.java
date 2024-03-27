8553c0f7d482e2572789753d81895a6cd4fa96bd
a.alenin <a.alenin@astondevs.ru>
2024-03-27 17:23:49 +0300

    commit new type salary

--- src/main/java/MoodleAQAJavaCourse/Lesson4.java
+++ src/main/java/MoodleAQAJavaCourse/Lesson4.java
@@ -1,19 +1,13 @@
-package MoodleAQAJavaCourse;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Lesson4 {
 
     public static void main(String[] args) {
 
         Person[] person = new Person[5];
-        person[0] = new Person("Alenin Andrew","AQA","","+79315762030","10000000",35);
-        person[1] = new Person("Karoeva Katerina","AQA","","+79219304558","10000000",40);
-        person[2] = new Person("Alexeev Alexey","DEV","","+79814562978","10000000",45);
-        person[3] = new Person("Alexandrov Alex","AQA","","+78922564815","10000000",56);
-        person[4] = new Person("Stepanov Stepan","DEV","","+78114562301","10000000",78);
+        person[0] = new Person("Alenin Andrew","AQA","","+79315762030",1000000,35);
+        person[1] = new Person("Karoeva Katerina","AQA","","+79219304558",500000,40);
+        person[2] = new Person("Alexeev Alexey","DEV","","+79814562978",120000,45);
+        person[3] = new Person("Alexandrov Alex","AQA","","+78922564815",5000000,56);
+        person[4] = new Person("Stepanov Stepan","DEV","","+78114562301",600000,78);
 
 //        if(person[0].age > 40){
 //            System.out.println(person[0].getFIO());
@@ -27,10 +21,10 @@ public class Lesson4 {
         String position;
         String email;
         String phone;
-        String salary;
+        Integer salary;
         Integer age;
 
-        public Person(String FIO, String position, String email, String phone, String salary, Integer age) {
+        public Person(String FIO, String position, String email, String phone, Integer salary, Integer age) {
             this.FIO = FIO;
             this.position = position;
             this.email = email;
