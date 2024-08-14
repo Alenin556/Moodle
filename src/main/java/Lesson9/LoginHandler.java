e6ec47cca6ac9664912ab638e01c67293fe1dc80
a.alenin <a.alenin@astondevs.ru>
2024-08-14 09:01:25 +0300

    Урок 9 : отредактирован метод фильтрации добавления логинов

--- src/main/java/Lesson9/LoginHandler.java
+++ src/main/java/Lesson9/LoginHandler.java
@@ -10,7 +10,6 @@ public class LoginHandler {
     //5. Нужно написать программу, которая будет принимать от пользователя ввод различных логинов.
     // Как только пользователь введет пустую строку - программа должна прекратить приём данных от пользователя
     // и вывести в консоль логины, начинающиеся на букву f (строчную).
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -20,17 +19,35 @@ public class LoginHandler {
         System.out.println("Введите login =)");
         String login;
 
+//        do {
+//            login = sc.nextLine();
+//            loginList.add(login);
+//            System.out.println("Login : " + login);
+//            System.out.println("Введите login =)");
+//            if (login.equals("")) {
+//                List<String> fLoginsStartWith = loginList.stream().filter(n -> n.startsWith("f")).collect(Collectors.toList());
+//                System.out.println(fLoginsStartWith);
+//            }
+//        }
+//        while (!login.equals(""));
+
+
+        List<String> fLoginsStartWith = new ArrayList<>();
         do {
             login = sc.nextLine();
-            loginList.add(login);
-            System.out.println("Login : " + login);
-            System.out.println("Введите login =)");
-            if (login.equals("")) {
-                List<String> fLoginsStartWith = loginList.stream().filter(n -> n.startsWith("f")).collect(Collectors.toList());
-                System.out.println(fLoginsStartWith);
+            if(login.startsWith(" ")){
+                break;
+            } else {
+                loginList.add(login);
+                System.out.println("Login : " + login);
+                System.out.println("Введите login =)");
+                fLoginsStartWith = loginList.stream().filter(n -> n.startsWith("f")).collect(Collectors.toList());
+                //если значение последнего логина null тогда конец
             }
         }
-        while (!login.equals(""));
+        while (!(login.length() == 0));
+        System.out.println(fLoginsStartWith);
+        System.out.println(loginList);
     }
 
 
