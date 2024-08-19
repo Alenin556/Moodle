7b108b0fe2054f8db6dc61ca808a8e6ca89ed7f9
a.alenin <a.alenin@astondevs.ru>
2024-08-19 08:23:59 +0300

    Урок 12 : практическое задание (доработки)

--- src/test/java/FactorialTestJ.java
+++ src/test/java/FactorialTestJ.java
@@ -19,15 +19,36 @@ public class FactorialTestJ {
         Integer factor = getFactorial(nums);
         Integer result = 3628800;
 
-        Assert.assertEquals(result,factor);
+        Assert.assertEquals(factor,result);
     }
 
     @Test
-    public void getFactorialLengthTest(){
+    public void getFactorialNegativeTest(){
+        //TODO 2) Нет обработки отрицательных значений в методе , и  тестирование этой обработки
+        Integer[] nums = new Integer[]{-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+
+        Integer factor = getFactorial(nums);
+        Integer result = null;
+
+        Assert.assertEquals(factor,result);
+    }
+
+    @Test
+    public void getFactorialResultLengthTest(){
+        //TODO 3) нет обработки выхода за пределы диапазона integer
+        Integer[] nums = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+
+        Integer factor = getFactorial(nums);
+        Integer result = null;
+
+        Assert.assertEquals(factor,result);
+    }
+    @Test
+    public void getFactorialEmptyLengthTest(){
         Integer[] nums = new Integer[]{};
 
         Integer factor = getFactorial(nums);
 
-        assertEquals(null,factor);
+        assertEquals(factor,null);
     }
 }
