a2e1ded33c5ac8f20fbbc98bfb83f8c74615d9b6
a.alenin <a.alenin@astondevs.ru>
2024-08-19 08:25:13 +0300

    Урок 12 : практическое задание (доработки)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,30 +6,42 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] nums = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        Integer[] nums = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         getFactorial(nums);
     }
 
     public static String successNotification = "Array isEmpty !";
     public static String resultNotification = "Result : ";
+    public static Integer intLength = 2147483647;
 
 
     // TODO получить на вход список массив чисел
     //  факториал это произведение всех чисел до этого числа
     //  отдать результат
-    public static Integer getFactorial(Integer[] numbers){
+    public static Integer getFactorial(Integer[] numbers) {
         Integer result = 1;
 
-        if (numbers.length == 0){
+        if (numbers.length == 0) {
             System.out.println(successNotification);
             return null;
         } else {
-            for(Integer x : numbers){
-                result*=x;
+            for (Integer x : numbers) {
+                //TODO Нет обработки отрицательных значений в методе , и  тестирование этой обработки
+                if (x < 0) {
+                    System.out.println("To calculate the factorial, the number cannot be less than zero");
+                    return null;
+                } else {
+                    result *= x;
+                }
             }
-            System.out.println(resultNotification + result);
-            return result;
+            if(result.toString().startsWith("-")){
+                System.out.println("The result is more Integer type value");
+                return null;
+            } else {
+                System.out.println(resultNotification + result);
+                return result;
+            }
         }
     }
 }
--- src/test/java/FactorialTestJ.java
+++ src/test/java/FactorialTestJ.java
@@ -3,25 +3,47 @@ import org.junit.jupiter.api.Test;
 
 import static org.example.Main.getFactorial;
 import static org.example.Main.successNotification;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTestJ {
-
 
     @Test
     public void getFactorialTest(){
         Integer[] nums = new Integer[]{1,2,3,4,5,6,7,8,9,10};
 
         Integer factor = getFactorial(nums);
+        Integer result = 3628800;
 
-        Assertions.assertEquals(3628800,factor);
+        assertEquals(factor,result);
     }
 
     @Test
-    public void getFactorialTypeTest(){
+    public void getFactorialNegativeTest(){
+        //TODO 2) Нет обработки отрицательных значений в методе , и  тестирование этой обработки
+        Integer[] nums = new Integer[]{-1,-2,-3,-4,-5,-6,-7,-8,-9,-10};
+
+        Integer factor = getFactorial(nums);
+        Integer result = null;
+
+        assertEquals(factor,result);
+    }
+
+    @Test
+    public void getFactorialResultLengthTest(){
+        //TODO 3) нет обработки выхода за пределы диапазона integer
+        Integer[] nums = new Integer[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+
+        Integer factor = getFactorial(nums);
+        Integer result = null;
+
+        assertEquals(factor,result);
+    }
+    @Test
+    public void getFactorialEmptyLengthTest(){
         Integer[] nums = new Integer[]{};
 
         Integer factor = getFactorial(nums);
 
-        Assertions.assertEquals(null,factor);
+        assertEquals(factor,null);
     }
 }
